60ab3d1b3230f0584e293dcce79c42f79e9b6d37
Hoshinonono <96542825+Hoshinonono@users.noreply.github.com>
2023-12-31 13:18:56 +0900

    Add files via upload

---
+++ Test/yukicoder/yuki2277.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2277"
+
+#include <bits/stdc++.h>
+#include "Graph/dsu_xor.hpp"
+#include "Math/modint.hpp"
+using mint = prime_modint<998244353>;
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int N, Q;
+    std::cin >> N >> Q;
+    xor_dsu<int> uf(N);
+    bool flg = true;
+    while(Q--){
+        int u, v, d;
+        std::cin >> u >> v >> d;
+        flg &= uf.merge(--u, --v, d);
+    }
+    std::cout << (flg ? mint(2).pow(uf.size()) : 0) << '\n';
+}

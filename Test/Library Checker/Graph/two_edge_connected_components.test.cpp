a06858fbefd09ca08b474f41a360681690f524e3
Hoshinonono <96542825+Hoshinonono@users.noreply.github.com>
2024-01-08 02:01:46 +0900

    Add files via upload

---
+++ Test/Library Checker/Graph/two_edge_connected_components.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/two_edge_connected_components"
+
+#include <bits/stdc++.h>
+#include "Graph/lowlink_tecc.hpp"
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, m, u, v;
+    cin >> n >> m;
+    vector<vector<int>> g(n);
+    while(m--){
+        cin >> u >> v;
+        g[u].emplace_back(v);
+        g[v].emplace_back(u);
+    }
+    auto ans = tecc_graph(g).tecc().second;
+    cout << ans.size() << '\n';
+    for(auto &&G : ans){
+        cout << G.size();
+        for(auto &&v : G) cout << ' ' << v;
+        cout << '\n';
+    }
+}

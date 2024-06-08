759b97db51c00c9a217a42545ec47a2b1bf4e5c9
Hoshinonono <96542825+Hoshinonono@users.noreply.github.com>
2024-06-08 17:14:04 +0900

    Add files via upload

---
+++ Test/yukicoder/yuki2780.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2780"
+
+#include <bits/stdc++.h>
+#include "Graph/scc.hpp"
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n);
+    for(int i = 0; i < n; i++){
+        int m;
+        cin >> m;
+        g[i].resize(m);
+        for(auto &&v : g[i]){
+            cin >> v;
+            v--;
+        }
+    }
+    SCC scc(g);
+    auto G = scc.groups();
+    vector<int> dp(G.size());
+    for(int i = 0; i < G.size(); i++){
+        if(dp[i] != i){
+            cout << "No\n";
+            return 0;
+        }
+        for(auto &&v : G[i]){
+            for(auto &&u : g[v]){
+                if(scc[v] == scc[u]) continue;
+                dp[scc[u]] = max(dp[scc[u]], dp[scc[v]] + 1);
+            }
+        }
+    }
+    cout << "Yes\n" << '\n';
+}
